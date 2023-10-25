368fb68180b4c2b49c5557345c6666a204fec5c8
Josef <98840857+Botcoinista@users.noreply.github.com>
2023-10-25 13:57:28 +0200

    added map

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,92 +1,150 @@
 "use client";
 
-import useRentModal from '@/app/hooks/useRentModl'
-import Modal from './Modal'
-import { useMemo, useState } from 'react';
-import Heading from '../Heading';
-import { categories } from '../navbar/Categories';
-import CategoryInput from '../inputs/CategoryInput';
+import useRentModal from "@/app/hooks/useRentModl";
+import Modal from "./Modal";
+import { useMemo, useState } from "react";
+import Heading from "../Heading";
+import { categories } from "../navbar/Categories";
+import CategoryInput from "../inputs/CategoryInput";
+import { FieldValues, set, useForm } from "react-hook-form";
+import CountrySelect from "../inputs/CountrySelect";
+import dynamic from "next/dynamic";
+
 
 enum STEPS {
-    CATEGORY = 0,
-    LOCATION = 1,
-    INFO = 2,
-    IMAGES = 3,
-    DESCRIPTION = 4,
-    PRICE = 5,
+  CATEGORY = 0,
+  LOCATION = 1,
+  INFO = 2,
+  IMAGES = 3,
+  DESCRIPTION = 4,
+  PRICE = 5,
 }
 
 const RentModal = () => {
-    const rentModal = useRentModal()
+  const rentModal = useRentModal();
 
-    const [step, setStep] = useState(STEPS.CATEGORY)
-    
-    const onBack = () => {
-        setStep((step) => step - 1)
+  const [step, setStep] = useState(STEPS.CATEGORY);
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      category: "",
+      location: null,
+      guestCount: 1,
+      roomCount: 1,
+      bathroomCount: 1,
+      imageSrc: "",
+      price: 1,
+      title: "",
+      description: "",
+    },
+  });
+
+  const category = watch("category");
+  const location = watch("location");
+
+  const Map = useMemo(() => dynamic(() => import('../Map'), { 
+    ssr: false 
+  }), [location]);
+
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldValidate: true,
+      shouldDirty: true,
+      shouldTouch: true,
+    });
+  };
+
+  const onBack = () => {
+    setStep((step) => step - 1);
+  };
+
+  const onNext = () => {
+    setStep((step) => step + 1);
+  };
+
+  const actionLabel = useMemo(() => {
+    if (step === STEPS.PRICE) {
+      return "Create";
+    }
+    return "Next";
+  }, [step]);
+
+  const secondaryActionLabel = useMemo(() => {
+    if (step === STEPS.CATEGORY) {
+      return undefined;
     }
 
-    const onNext = () => {
-        setStep((step) => step + 1)
-    }
+    return "Back";
+  }, [step]);
 
-    const actionLabel = useMemo(() => {
-        if (step === STEPS.PRICE) {
-            return 'Create'
-        }
-        return 'Next'
-    }, [step])
-
-    const secondaryActionLabel = useMemo(() => {
-        if (step === STEPS.CATEGORY) {
-            return undefined
-        }
-
-        return 'Back'
-    }, [step])
-
-    let bodyContent = (
-        <div className='flex flex-col gap-8'>
-            <Heading
-                title="Which category best describes your listing?"
-                subtitle='Pick a category that best suits the place you’re renting out.'
-            />
-            <div
-                className='
+  let bodyContent = (
+    <div className="flex flex-col gap-8">
+      <Heading
+        title="Which category best describes your listing?"
+        subtitle="Pick a category that best suits the place you’re renting out."
+      />
+      <div
+        className="
                 grid
                 grid-cols-1
                 md:grid-cols-2
                 gap-3
                 max-h-[50vh]
                 overflow-y-auto
-                '
-                >
-                    {categories.map((item) => (
-                        <div key={item.label} className='col-span-1'>
-                            <CategoryInput
-                            onClick={() => {}}
-                            selected={false}
-                            label={item.label}
-                            icon={item.icon}
-                         />
-                        </div>
+                "
+      >
+          {categories.map((item) => (
+          <div key={item.label} className="col-span-1">
+            <CategoryInput
+              onClick={(category) => 
+                setCustomValue('category', category)}
+              selected={category === item.label}
+              label={item.label}
+              icon={item.icon}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 
-                    ))}
-            </div>
-        </div>
-    )
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Where’s your place located?"
+          subtitle="Guests will only get your exact address after they’ve booked a reservation."
+        />
+        <CountrySelect
+        value={location}
+          onChange={(value) => setCustomValue("location", value)}
+        />
+        <Map
+            center={location?.latlng} 
+            />
+      </div>
+    );
+  }
 
   return (
     <Modal
-    isOpen={rentModal.isOpen}
-    onClose={rentModal.onClose}
-    onSubmit={rentModal.onClose}
-    actionLabel={actionLabel}
-    secondaryActionLabel={secondaryActionLabel}
-    secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
-    title="Airbnb your home!"
-    body={bodyContent}
+      isOpen={rentModal.isOpen}
+      onClose={rentModal.onClose}
+      onSubmit={onNext}
+      actionLabel={actionLabel}
+      secondaryActionLabel={secondaryActionLabel}
+      secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
+      title="Airbnb your home!"
+      body={bodyContent}
     />
-  )
-}
+  );
+};
 
-export default RentModal
+export default RentModal;
