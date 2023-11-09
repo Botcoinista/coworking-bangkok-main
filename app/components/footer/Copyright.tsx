31970d25658f5f793d6c55655b502a8bb9806d3b
Josef Reinhold <josef.r@hotmail.se>
2023-11-08 17:21:26 -0800

    You are not logged in

--- app/components/footer/Copyright.tsx
+++ app/components/footer/Copyright.tsx
@@ -4,7 +4,7 @@ const Copyright = () => {
   return (
     <div className="flex justify-center items-center gap-4">
       <div>
-        <FaCopyright size={40} />
+        <FaCopyright size={30} />
       </div>
       <div>CO-WORKING Bangkok 2023</div>
     </div>
