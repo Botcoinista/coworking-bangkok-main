d5251fa19058cda35060d6ad05a4dd78c2a3209d
Josef Reinhold <josef.r@hotmail.se>
2023-11-24 14:03:37 +0100

    fixed a bug in loginmodal

--- app/components/ReservationButton.tsx
+++ app/components/ReservationButton.tsx
@@ -1,59 +1,67 @@
-
-import useConfirmationModal from '../hooks/useConfirmationModal';
-import Button from './Button'
-
-
+'use client';
+import { useRouter } from "next/navigation";
+import useConfirmationModal from "../hooks/useConfirmationModal";
+import Button from "./Button";
+import useLoginModal from "../hooks/useLoginModal";
+import useCheckoutModal from "../hooks/useCheckoutModal";
 interface ReservationButtonProps {
-    disabled: boolean;
-    totalPrice: number;
-    onSubmit: () => void;
-    }
-
-
-const ReservationButton = ({ disabled, totalPrice, onSubmit }: ReservationButtonProps) => {
-
-    const { isOpen, onOpen, onClose } = useConfirmationModal();
-
-    const handleButtonClick = () => {
+  totalPrice: number;
+  disabled: boolean;
+  onSubmit: () => void;
+  currentUser: any;
+}
+const ReservationButton = ({
+  totalPrice,
+  disabled,
+  onSubmit,
+  currentUser,
+}: ReservationButtonProps) => {
+  const { isOpen, onOpen, onClose } = useConfirmationModal();
+  const loginModal = useLoginModal();
+  const checkoutModal = useCheckoutModal();
+  const handleButtonClick = () => {
+    // Check if the user is logged in
+    if (currentUser) {
       // Open the confirmation modal
       onOpen();
       // Call the onSubmit function after a delay (you can adjust the delay as needed)
       setTimeout(() => {
         onSubmit();
         onClose();
-      }, 3000); 
-    };
-
+      }, 3000); // Example delay of 3000 milliseconds
+    } else {
+      // User is not logged in, close the checkout modal first
+      checkoutModal.onClose();
+      // Use a callback or setTimeout to open the login modal after closing checkout modal
+      setTimeout(() => {
+        loginModal.onOpen();
+      }, 100); // Example delay of 500 milliseconds
+    }
+  };
   return (
     <>
-    
-<div className="
-py-2
-flex
-flex-row
-items-center
-justify-between
-font-semibold
-text-lg
-font-poppins
-">
-    <div>
-        Total
-    </div>
-    <div>
-         {totalPrice} THB
-    </div>
-
-</div>
-    <div className="">
-    <Button 
-        disabled={disabled}
-        label="Book now"
-        onClick={handleButtonClick}
-    />
-</div>
+      <div
+        className="
+        flex
+        flex-row
+        items-center
+        justify-between
+        py-2
+        font-semibold
+        text-lg
+        font-poppins
+      ">
+        <div>Total</div>
+        <div>{totalPrice} THB</div>
+      </div>
+      <div className="">
+        <Button
+          disabled={disabled}
+          label="Book now"
+          onClick={handleButtonClick}
+        />
+      </div>
     </>
-  )
-}
-
-export default ReservationButton
+  );
+};
+export default ReservationButton;
